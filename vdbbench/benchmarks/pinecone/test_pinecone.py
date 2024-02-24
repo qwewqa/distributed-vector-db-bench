3ce603527370aec933c21656b882ec4a9d8ff426
Vedant J <vedantjh2@gmail.com>
2024-02-24 15:42:21 -0800

    initialize pinecone directory and add basic code

---
+++ vdbbench/benchmarks/pinecone/test_pinecone.py
@@ -0,0 +1,27 @@
+from pinecone import Pinecone, ServerlessSpec
+# TODO: change structure and make it functional
+pc = Pinecone(api_key='YOUR_API_KEY')
+
+pc.create_index(
+    name="quickstart",
+    dimension=8,
+    metric="euclidean",
+    # change later, serverless only available on AWS 
+    # trying to start with some boilerplate code rn git commit -am ""
+    spec=ServerlessSpec(
+        cloud='aws', 
+        region='us-west-2'
+    ) 
+) 
+
+index = pc.Index("quickstart")
+
+index.upsert(
+  vectors=[
+    {"id": "vec1", "values": [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]},
+    {"id": "vec2", "values": [0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2]},
+    {"id": "vec3", "values": [0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3]},
+    {"id": "vec4", "values": [0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4]}
+  ],
+  namespace="ns1"
+)
