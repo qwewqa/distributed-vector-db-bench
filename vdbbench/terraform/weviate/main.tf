14faa8e60d96744fefe3f7e4e8d7232b5eeffba1
yejin <i3pivigui@icloud.com>
2024-03-21 18:49:42 -0700

    implemented main.tf file for weaviate

--- vdbbench/terraform/weviate/main.tf
+++ vdbbench/terraform/weviate/main.tf
@@ -2,47 +2,126 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 3.5"
+      version = "5.20.0"
     }
   }
-
-  required_version = ">= 0.12"
 }
 
 provider "google" {
-  credentials = file("<PATH-TO-YOUR-CREDENTIALS-FILE>")
-  project     = "<YOUR-GCP-PROJECT-ID>"
-  region      = "us-central1"
+  credentials = var.credentials != "" ? file(var.credentials) : null
+
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
 
-resource "google_compute_instance" "weaviate_instance" {
-  name         = "weaviate-instance"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
+resource "google_compute_network" "default" {
+  name = "elasticsearch-network"
+}
+
+resource "google_compute_firewall" "ssh" {
+  name    = "elasticsearch-firewall-ssh"
+  network = google_compute_network.default.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "internal" {
+  name    = "elasticsearch-firewall-internal"
+  network = google_compute_network.default.name
+
+  allow {
+    protocol = "all"
+  }
+
+  source_tags = ["elasticsearch"]
+}
+
+resource "google_compute_instance" "db_instances" {
+  for_each     = toset([for i in range(var.node_count) : tostring(i)])
+  name         = "elasticsearch-${each.key}"
+  machine_type = var.machine_type
+  tags         = ["elasticsearch"]
+  allow_stopping_for_update = true
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      type = "pd-ssd"
+      size = 128
     }
   }
 
   network_interface {
-    network = "default"
+    network = google_compute_network.default.name
     access_config {
       // Ephemeral IP
     }
   }
 
+  metadata = {
+    ssh-keys = "${var.ssh_user}:${var.ssh_public_key}"
+  }
+
   metadata_startup_script = <<-EOF
-    #!/bin/bash
-    sudo apt-get update
-    sudo apt-get install -y docker.io
-    sudo systemctl start docker
-    sudo systemctl enable docker
-    sudo docker run -d --name weaviate -p 8080:8080 semitechnologies/weaviate:1.13.2
-    EOF
+        #!/bin/bash
+        wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
+        echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
+        sudo apt-get update
+        sudo apt-get install elasticsearch -y
+
+        sudo cat <<EOT > /etc/elasticsearch/elasticsearch.yml
+        path.data: "/var/lib/elasticsearch"
+        path.logs: "/var/log/elasticsearch"
+        xpack.security.enabled: false
+        xpack.security.autoconfiguration.enabled: false
+        http.host: 0.0.0.0
+        cluster.name: "elasticsearch"
+        node.name: "elasticsearch-${each.key}"
+        network.host: 0.0.0.0
+        discovery.seed_hosts: ["${join("\", \"", [for i in range(var.node_count) : "elasticsearch-${i}"])}"]
+        cluster.initial_master_nodes: ["${join("\", \"", [for i in range(var.node_count) : "elasticsearch-${i}"])}"]
+        EOT
+
+        # As recommended by Elasticsearch, disable swap for performance: https://www.elastic.co/guide/en/elasticsearch/reference/current/setup-configuration-memory.html
+        sudo swapoff -a
+
+        # Remove the default keystore since we're disabling security
+        sudo rm /etc/elasticsearch/elasticsearch.keystore
+        sudo /usr/share/elasticsearch/bin/elasticsearch-keystore create
+
+        ${var.before_start}
+
+        sudo systemctl start elasticsearch
+        sudo systemctl enable elasticsearch
+        EOF
 }
 
-output "instance_ip" {
-  value = google_compute_instance.weaviate_instance.network_interface[0].access_config[0].nat_ip
+resource "google_compute_instance" "runner_instance" {
+  name         = "elasticsearch-runner"
+  machine_type = var.machine_type
+  tags         = ["elasticsearch"]
+  allow_stopping_for_update = true
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.default.name
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata = {
+    ssh-keys = "${var.ssh_user}:${var.ssh_public_key}"
+  }
 }
