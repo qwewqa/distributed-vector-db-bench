c798b6994c8d79e37ad0b140dfbbd9ff7aae3dc9
Vedant J <vedantjh2@gmail.com>
2024-04-02 18:07:34 -0700

    changes to start incorporating code from notebook to a python script

--- vdbbench/benchmarks/pinecone/test_pinecone.py
+++ vdbbench/benchmarks/pinecone/test_pinecone.py
@@ -1,8 +1,11 @@
 from pinecone import Pinecone, ServerlessSpec
-# TODO: change structure and make it functional
+from pinecone_datasets import list_datasets, load_dataset
 from constants import PINECONE_API_KEY
-from datasets import DATASETS
+import pandas as pd
+import numpy as np
+import time
 
+glove100_dataset = load_dataset("ANN_GloVe_d100_angular")
 
 pc = Pinecone(api_key=PINECONE_API_KEY)
 pc.create_index(
@@ -17,17 +20,66 @@ pc.create_index(
     ) 
 ) 
 
-glove100 = DATASETS["glove-100d"]
+index = pc.Index("glove100d")
 
 
-index = pc.Index("quickstart")
+nn = glove100_dataset.queries["blob"][0]["nearest_neighbors"]
 
-index.upsert(
-  vectors=[
-    {"id": "vec1", "values": [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]},
-    {"id": "vec2", "values": [0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2]},
-    {"id": "vec3", "values": [0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3]},
-    {"id": "vec4", "values": [0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4]}
-  ],
-  namespace="ns1"
-)
+dataset = glove100_dataset.documents
+def upload_data(dataset=dataset, index=index):
+    print(f"\n Uploading Data...")
+    start = time.perf_counter()
+    index.upsert_from_dataframe(dataset.drop(columns=["sparse_values","metadata","blob"]))
+    end = time.perf_counter()
+    return (end-start)/60.0
+
+upload_latency = upload_data()
+batch_size = 100
+query_vectors = np.asarray(glove100_dataset.queries["vector"])
+query_results = glove100_dataset.queries["blob"]
+
+def query(query_vectors=query_vectors, index=index, k=100):
+    print(f"\n Batch Querying...")
+    batch_times = []
+    results = []
+    for i in range(0,10000, batch_size):
+        batch_items = query_vectors[i:i+batch_size]
+        start = time.perf_counter()
+        # query_results = index.query(queries=batch_items, top_k=10, disable_progress_bar=True)
+        for item in batch_items:
+            res = index.query(vector=item, top_k=k, include_values=True)
+            results.append(res)
+        end = time.perf_counter()
+        batch_times.append(end-start)
+        print("batch done", i)
+    return [batch_times, results]
+
+
+times, results = query()
+
+def formatResults(results):
+	formatted_results = []
+		# result["matches"]
+		# id = result["matches"]["id"]
+		# score = result["matches"]["score"]
+		# values = result["matches"]["values"]
+		# x = {"id":id, "score":score, "values":values}
+	for result in results:
+		matches = result["matches"]
+		res_matches = []
+		for match in matches:
+			id = match["id"]
+			score = match["score"]
+			values = match["values"]
+			x = {"id":id, "score":score, "values":values}
+			res_matches.append(x)
+		formatted_results.append(res_matches)
+    
+	return formatted_results
+
+formatted_results = formatResults(results)
+
+nn100 = []
+for x in glove100_dataset.queries["blob"]: 
+    s = set(x["nearest_neighbors"])
+    nn100.append(s)
