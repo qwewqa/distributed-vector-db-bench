c358109148f06add0483bb386ee4eae4864e613e
Vedant J <vedantjh2@gmail.com>
2024-04-04 17:21:08 -0700

    cleanup and write run function

--- vdbbench/benchmarks/pinecone/test_pinecone.py
+++ vdbbench/benchmarks/pinecone/test_pinecone.py
@@ -5,45 +5,23 @@ import pandas as pd
 import numpy as np
 import time
 
-glove100_dataset = load_dataset("ANN_GloVe_d100_angular")
+BATCH_SIZE = 100
 
-pc = Pinecone(api_key=PINECONE_API_KEY)
-pc.create_index(
-    name="glove100d",
-    dimension=100,
-    metric="cosine",
-    # change later, serverless only available on AWS 
-    # trying to start with some boilerplate code rn git commit -am ""
-    spec=ServerlessSpec(
-        cloud='aws', 
-        region='us-west-2'
-    ) 
-) 
-
-index = pc.Index("glove100d")
-
-
-nn = glove100_dataset.queries["blob"][0]["nearest_neighbors"]
-
-dataset = glove100_dataset.documents
-def upload_data(dataset=dataset, index=index):
+def upload_data(dataset, index):
     print(f"\n Uploading Data...")
     start = time.perf_counter()
     index.upsert_from_dataframe(dataset.drop(columns=["sparse_values","metadata","blob"]))
     end = time.perf_counter()
     return (end-start)/60.0
 
-upload_latency = upload_data()
-batch_size = 100
-query_vectors = [item.tolist() for item in glove100_dataset.queries["vector"]]
-query_results = glove100_dataset.queries["blob"]
 
-def query(query_vectors=query_vectors, index=index, k=100):
+
+def query(query_vectors, index, k=100):
     print(f"\n Batch Querying...")
     batch_times = []
     results = []
-    for i in range(0,10000, batch_size):
-        batch_items = query_vectors[i:i+batch_size]
+    for i in range(0,10000, BATCH_SIZE):
+        batch_items = query_vectors[i:i+BATCH_SIZE]
         start = time.perf_counter()
         # query_results = index.query(queries=batch_items, top_k=10, disable_progress_bar=True)
         for item in batch_items:
@@ -53,9 +31,6 @@ def query(query_vectors=query_vectors, index=index, k=100):
         batch_times.append(end-start)
         print("batch done", i)
     return [batch_times, results]
-
-
-times, results = query()
 
 def formatResults(results):
 	formatted_results = []
@@ -77,9 +52,34 @@ def formatResults(results):
     
 	return formatted_results
 
-formatted_results = formatResults(results)
 
-nn100 = []
-for x in glove100_dataset.queries["blob"]: 
-    s = set(x["nearest_neighbors"])
-    nn100.append(s)
+def run():
+	glove100_dataset = load_dataset("ANN_GloVe_d100_angular")
+
+	pc = Pinecone(api_key=PINECONE_API_KEY)
+	pc.create_index(
+		name="glove100d",
+		dimension=100,
+		metric="cosine",
+		# change later, serverless only available on AWS 
+		# trying to start with some boilerplate code rn git commit -am ""
+		spec=ServerlessSpec(
+			cloud='aws', 
+			region='us-west-2'
+		) 
+	) 
+
+	index = pc.Index("glove100d")
+
+	nn = glove100_dataset.queries["blob"][0]["nearest_neighbors"]
+
+	dataset = glove100_dataset.documents
+	upload_latency = upload_data(dataset, index)
+	query_vectors = [item.tolist() for item in glove100_dataset.queries["vector"]]
+	# query_results = glove100_dataset.queries["blob"]
+	times, results = query(query_vectors, index, 10)
+	formatted_results = formatResults(results)
+	nn100 = []
+	for x in glove100_dataset.queries["blob"]: 
+		s = set(x["nearest_neighbors"])
+		nn100.append(s)
